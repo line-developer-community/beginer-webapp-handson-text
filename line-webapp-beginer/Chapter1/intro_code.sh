087b27f17d1878b227674334ecd304015bd01991
Miura55 <acordion.piano@gmail.com>
2020-12-30 19:57:16 +0900

    fix setup directory

--- line-webapp-beginer/Chapter1/intro_code.sh
+++ line-webapp-beginer/Chapter1/intro_code.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Clone handson repository
-git clone https://github.com/line-developer-community/beginer-webapp-handson-code.git example
+git clone https://github.com/line-developer-community/beginer-webapp-handson-code.git handson
 
 # Install Python Libralies
-cd example
+cd handson
 pip3 install -r requirements.txt
 clear
 echo "Finish!"
